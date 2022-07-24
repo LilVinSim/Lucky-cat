2d0644d2c7679f3a3564f76403f53d16ceb8fa50
LilVinSim <109905857+LilVinSim@users.noreply.github.com>
2022-07-25 01:48:20 +1000

    Add files via upload
    updated app.js added

---
+++ app.js
@@ -0,0 +1,24 @@
+
+let luckyCat;
+function preload(){
+    var url = 'Maneki_Neko.OBJ'
+    luckyCat = loadModel(url)
+}
+function setup() {
+	var myCanvas = createCanvas(windowWidth, windowHeight, WEBGL);
+    const newLocal = 'p5Container';
+    myCanvas.parent(newLocal);
+}
+
+function windowResized(){
+	resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+    background(0);
+    normalMaterial();
+    camera(0, 0, -180 );
+    rotateX(280);
+    rotateY(frameCount * 0.01);
+    model(luckyCat);
+}
